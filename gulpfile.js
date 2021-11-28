cdf85dc4773e09b9461f4ebac001cd4878f53fdf
mariamapciaurii <mariamapciaurii@gmail.com>
2021-11-28 11:14:43 -0800

    feat: add shared header and basic page layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-'use strict';
 
+
+//depemdemcies
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
-function buildStyles() {
-  return gulp.src('./sass/**/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
+// ./scss/*.sscs
 
-exports.buildStyles = buildStyles;
+// ['js/**/*.js', '!js/**/*.min.js']
+
+//Styles tasks
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss') // Gets all files ending with .scss in /scss and children dirs
+      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(gulp.dest('./css'))
+});
+
+
+//Gulp Watch syntax has changed from 3.x in 4.x
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
